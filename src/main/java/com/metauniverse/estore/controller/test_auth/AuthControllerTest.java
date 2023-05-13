7542ad487ea329967cce8da324621a0211022d72
kolgotik <90344391+kolgotik@users.noreply.github.com>
2023-05-13 22:45:19 +0500

    minor changes

--- src/main/java/com/metauniverse/estore/controller/test_auth/AuthControllerTest.java
+++ src/main/java/com/metauniverse/estore/controller/test_auth/AuthControllerTest.java
@@ -1,7 +1,10 @@
 package com.metauniverse.estore.controller.test_auth;
 
+import com.metauniverse.estore.user.User;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +13,18 @@ public class AuthControllerTest {
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/auth")
-    public String testAuth(){
-        return "Authenticated " + SecurityContextHolder.getContext().getAuthentication().getName();
+    public String testAuth(@AuthenticationPrincipal OAuth2User oAuth2User, @AuthenticationPrincipal User user) {
+
+        String username;
+
+        if (oAuth2User != null) {
+            username = oAuth2User.getAttribute("name");
+        } else if (user != null) {
+            username = user.getUsername();
+        } else {
+            username = "Oops! Something is wrong";
+        }
+        return "Authenticated " + username;
     }
 
 }
