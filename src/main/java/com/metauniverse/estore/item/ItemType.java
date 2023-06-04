cc6602c02dbb47ef545df19733f432a9c46c973e
kolgotik <90344391+kolgotik@users.noreply.github.com>
2023-06-04 17:23:07 +0500

    added pages for every category

---
+++ src/main/java/com/metauniverse/estore/item/ItemType.java
@@ -0,0 +1,18 @@
+package com.metauniverse.estore.item;
+
+import lombok.Getter;
+
+@Getter
+public enum ItemType {
+    ANDROID_SMARTPHONE("android smartphone"),
+    APPLE_SMARTPHONE("apple smartphone"),
+    DESKTOP_PC("desktop pc"),
+    LAPTOP("laptop"),
+    PC_COMPONENT("pc component"),
+    CONSOLE("console");
+    private final String value;
+
+    ItemType(String value) {
+        this.value = value;
+    }
+}
--- src/main/java/com/metauniverse/estore/item/controller/CategoryController.java
+++ src/main/java/com/metauniverse/estore/item/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.metauniverse.estore.item.controller;
 
 import com.metauniverse.estore.item.Item;
 import com.metauniverse.estore.item.ItemRepository;
+import com.metauniverse.estore.item.ItemType;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,27 +24,37 @@ public class CategoryController {
     }
 
     @GetMapping("/smartphones")
-    public String getSmartphones() {
+    public String getSmartphones(Model model) {
+        Iterable<Item> smartphones = itemRepository.getItemsByType(ItemType.ANDROID_SMARTPHONE.getValue());
+        model.addAttribute("smartphones", smartphones);
         return "categories-smartphones";
     }
 
     @GetMapping("/laptops")
-    public String getLaptops() {
+    public String getLaptops(Model model) {
+        Iterable<Item> laptops = itemRepository.getItemsByType(ItemType.LAPTOP.getValue());
+        model.addAttribute("laptops", laptops);
         return "categories-laptops";
     }
 
     @GetMapping("/desktop-pcs")
-    public String getDesktopPCs() {
+    public String getDesktopPCs(Model model) {
+        Iterable<Item> pcs = itemRepository.getItemsByType(ItemType.DESKTOP_PC.getValue());
+        model.addAttribute("pcs", pcs);
         return "categories-desktop-pcs";
     }
 
     @GetMapping("/pc-components")
-    public String getPCComponents() {
+    public String getPCComponents(Model model) {
+        Iterable<Item> pcComponents = itemRepository.getItemsByType(ItemType.PC_COMPONENT.getValue());
+        model.addAttribute("pcComponents", pcComponents);
         return "categories-pc-components";
     }
 
     @GetMapping("/consoles")
-    public String getConsoles() {
+    public String getConsoles(Model model) {
+        Iterable<Item> consoles = itemRepository.getItemsByType(ItemType.CONSOLE.getValue());
+        model.addAttribute("consoles", consoles);
         return "categories-consoles";
     }
 }
