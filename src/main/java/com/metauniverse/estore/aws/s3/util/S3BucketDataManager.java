f87113eb36809496cd49f96458c5a11fd38dab60
kolgotik <90344391+kolgotik@users.noreply.github.com>
2023-06-25 00:45:19 +0500

    now according item image is displayed everywhere

--- src/main/java/com/metauniverse/estore/aws/s3/util/S3BucketDataManager.java
+++ src/main/java/com/metauniverse/estore/aws/s3/util/S3BucketDataManager.java
@@ -1,9 +1,16 @@
 package com.metauniverse.estore.aws.s3.util;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.metauniverse.estore.item.Item;
+import jakarta.servlet.http.HttpSession;
+
+import java.util.List;
+import java.util.Map;
 
 public interface S3BucketDataManager {
     String getObjectImageLink(Long itemId, AmazonS3 s3client);
+
+    Map<Long ,String> getObjectsImageLinks(List<Item> items, AmazonS3 s3client, HttpSession session);
 
     void putObjectImageLink(Long itemId, String imgLink, AmazonS3 s3client);
 }
