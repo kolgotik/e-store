ba2bcd7efd9a76a1269d9f35d090241f29e1317b
kolgotik <90344391+kolgotik@users.noreply.github.com>
2023-06-30 17:23:46 +0500

    Now user can top up the balance

--- src/main/java/com/metauniverse/estore/user/UserController.java
+++ src/main/java/com/metauniverse/estore/user/UserController.java
@@ -1,21 +1,27 @@
 package com.metauniverse.estore.user;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/profile")
 @AllArgsConstructor
+@PreAuthorize("isAuthenticated()")
 public class UserController {
 
     private final UserRepository userRepository;
+
     @GetMapping
     public String showProfilePage(Model model, @AuthenticationPrincipal OAuth2User oAuth2User, @AuthenticationPrincipal User user) {
         String email = UserService.getUsernameOfAuthUser(user, oAuth2User);
@@ -25,6 +31,22 @@ public class UserController {
             model.addAttribute("user", authUser);
         }
         return "profile";
+    }
+
+    @PostMapping("/add-balance")
+    public String topUpBalance(@RequestParam("balance") BigDecimal balance, Model model, @AuthenticationPrincipal OAuth2User oAuth2User, @AuthenticationPrincipal User user) {
+        String email = UserService.getUsernameOfAuthUser(user, oAuth2User);
+        Optional<User> optionalUser = userRepository.findByEmail(email);
+        if (optionalUser.isPresent()) {
+            User authUser = optionalUser.get();
+            BigDecimal currentBalance = authUser.getBalance();
+            currentBalance = currentBalance.add(balance);
+            authUser.setBalance(currentBalance);
+            userRepository.save(authUser);
+            model.addAttribute("user", authUser);
+
+        }
+        return "redirect:/profile";
     }
 
 }
